417eb7ba757ad71b63609b317f682d3a0f800dd1
Sergio Nhaca <snhaca@hotmail.com>
2023-08-30 21:18:11 +0200

    feat: insert migration in project

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Post()
   async createUser(@Body() createUser: CreateUserDto) {
     return {
---
+++ src/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { UserEntity } from './interfaces/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { hash } from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const saltOrRounds = 10;
+
+    const passwordHashed = await hash(createUserDto.password, saltOrRounds);
+
+    return this.userRepository.save({
+      ...createUserDto,
+      typeUser: 1,
+      password: passwordHashed,
+    });
+  }
+
+  async getAllUser(): Promise<UserEntity[]> {
+    return this.userRepository.find();
+  }
+}
